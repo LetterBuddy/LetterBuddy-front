43ce87d46fe11a32e5efe659b8001f562b0b9e20
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2025-01-25 20:27:36 +0200

    refactor:  Adding react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import './App.css'
-import React from 'react'
-import Button from './components/ui/button/Button'
 import Layout from './components/layouts/Layout';
-import Label from './components/ui/label/Label';
-import Head from './components/layouts/head/Head';
-
-// TODO: Add all the app routes here 
+import { Routes, Route, Navigate} from "react-router-dom";
+import SplashPage from './pages/SplashPage';
 
 function App() {
-  const [isClicked, setIsClicked] = React.useState<boolean>(false);
-
-  const toggleButtonHandler = () => {
-    setIsClicked((oldState) => !oldState);
-  };
-
   return (
-    <>
-      <Head />
-      <Label>A personal AI assistant to help your kids improve their handwriting </Label>
-      <Button onClick={toggleButtonHandler} variant="default">GET STARTED</Button>
-      <Button onClick={toggleButtonHandler} variant="white">I already have an account</Button>
-    </>
+    <div>
+      <Layout >
+        <Routes>
+          <Route path="/Splash" element={<SplashPage />}></Route>
+          <Route path="/" element={<Navigate to="/Splash" />}></Route>
+        </Routes>
+      </Layout>
+    </div>
   )
 }
 
