883e4851f4104bb1ac30ff711f94f199f7a9b5f6
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2025-03-04 12:16:01 +0200

    Refactoring: general code fixing

--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -1,23 +1,27 @@
-// import React from 'react';
+//import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../components/ui/button/Button';
 import Label from '../components/ui/label/Label';
 import myPen from '../assets/myPen.gif';
 import EntryLayout from '../components/layouts/entryLayout/EntryLayout';
 
 const SplashPage = () => {
+    const navigate = useNavigate();
 
-    // const [isClicked, setIsClicked] = React.useState<boolean>(false);
-
-    const toggleButtonHandler = () => {
-        // Handle the choice here
-
+    const getStartedHandler = () => {
+        navigate('/userType');
     };
+
+    const alreadyHasAccountHandler = () => {
+        // Handle the choice here
+    };
+
     return (
         <EntryLayout>
             <Label>A personal AI assistant to help your kids improve their handwriting </Label>
             <img src={myPen} alt="myPen" style={{ width: '230px', height: '250px', margin: '1.5rem 1rem' }} />
-            <Button onClick={toggleButtonHandler} variant="default">GET STARTED</Button>
-            <Button onClick={toggleButtonHandler} variant="white">I already have an account</Button>
+            <Button onClick={getStartedHandler} variant="default">GET STARTED</Button>
+            <Button onClick={alreadyHasAccountHandler} variant="white">I already have an account</Button>
         </EntryLayout>
     );
 }
