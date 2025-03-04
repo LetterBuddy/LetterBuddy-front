f70cffc84f458c918c557f67db00a8e0b54692b3
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2025-03-04 12:21:18 +0200

    Refactoring: log userType

--- src/components/userType/UserTypeButtons.tsx
+++ src/components/userType/UserTypeButtons.tsx
@@ -10,6 +10,8 @@ const UserTypeButtons = () => {
 
   const userTypeHandler = (type: "adult" | "child") => {
     setUserType(type);
+    console.log(userType);
+    
     navigate("/auth");
   };
 
