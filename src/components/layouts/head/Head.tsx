5db9aa4d8607a589ebf394b0c6e8c8a22c211ae6
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2025-03-26 19:41:55 +0200

    Feat: logout button added

--- src/components/layouts/head/Head.tsx
+++ src/components/layouts/head/Head.tsx
@@ -1,35 +1,82 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { IconAbc, IconHome } from '@tabler/icons-react';
-import classes from './Head.module.css';
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  IconAbc,
+  IconHome,
+  IconArticleFilled,
+  IconLogout,
+} from "@tabler/icons-react";
+import useUserStore from "../../../store/useUserStore";
+import classes from "./Head.module.css";
+import axiosAPI from "../../../axiosAPI";
 
 const Head = () => {
-  const location = useLocation(); 
-  const navigate = useNavigate(); 
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isChild = useUserStore((state) => state.isChild);
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+  const clearUser = useUserStore((state) => state.clearUser);
 
-  const isSubmissionPage = location.pathname === '/submission';
-  const isAlphabetPage = location.pathname === '/alphabet';
+  const isSubmissionPage = location.pathname === "/submission";
+  const isAlphabetPage = location.pathname === "/alphabet";
+  const isChildAccountsPage = location.pathname === "/accounts";
+
+  const logoutHandler = async () => {
+    try {
+      await axiosAPI.post("/accounts/logout/", {
+        refresh_token: localStorage.getItem("refresh_token"),
+      });
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      clearUser();
+      navigate("/splash");
+    } catch (error: any) {
+      console.log("Logout Failed", error.response.data);
+    }
+  };
 
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>LetterBuddy</h1>
-      
-      {isSubmissionPage && (
-        <IconAbc 
-          className={classes.icon} 
-          size={45} 
-          onClick={() => navigate('/alphabet')} 
-        />
-      )}
-      
-      {isAlphabetPage && (
-        <IconHome 
-          className={classes.icon} 
-          size={35} 
-          onClick={() => navigate('/submission')} 
-        />
-      )}
+      <div className={classes.icons}>
+        {isSubmissionPage && (
+          <IconAbc
+            className={classes.icon}
+            size={45}
+            onClick={() => navigate("/alphabet")}
+          />
+        )}
+
+        {isAlphabetPage && (
+          <IconHome
+            className={classes.icon}
+            size={35}
+            onClick={() => navigate("/submission")}
+          />
+        )}
+        {!isChildAccountsPage && !isChild && isLoggedIn && (
+          <IconHome
+            className={classes.icon}
+            size={35}
+            onClick={() => navigate("/accounts")}
+          />
+        )}
+        {isChildAccountsPage && (
+          <IconArticleFilled
+            className={classes.icon}
+            size={35}
+            onClick={() => navigate("/articles")}
+          />
+        )}
+        {isLoggedIn && (
+          <IconLogout
+            className={classes.icon}
+            size={35}
+            onClick={() => logoutHandler()}
+          />
+        )}
+      </div>
     </header>
   );
-}
+};
 
 export default Head;
