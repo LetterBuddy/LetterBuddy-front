39d6d3fda20ade8b528e31b9fddc858db644f0bf
Erez Lavi <Erez.Lavi@s.afeka.ac.il>
2025-04-07 09:28:30 +0300

    isLoading state added to child list loading

--- src/components/adult/ChildList.tsx
+++ src/components/adult/ChildList.tsx
@@ -3,18 +3,23 @@ import classes from "./ChildList.module.css";
 import useChildStore from "../../store/useChildStore";
 import { useEffect } from "react";
 import axiosAPI from "../../axiosAPI";
+import useLoadingStore from "../../store/useLoadingStore";
 
 const ChildList = () => {
   const { children, setChildren } = useChildStore();
+  const { setIsLoading } = useLoadingStore();
 
   useEffect(() => {
     if (children.length > 0) return;
     const fetchChildren = async () => {
       try {
+        setIsLoading(true);
         const response = await axiosAPI.get("/accounts/child/");
         setChildren(response.data);
       } catch (error: any) {
         console.error("Failed to fetch children", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchChildren();
