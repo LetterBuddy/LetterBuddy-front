46e796ae7a88760d4bdcab534cdfa53cc2729f8b
Ofir Beck <ofirbeck987@gmail.com>
2025-04-07 21:06:22 +0300

    feat: added an option to hear out loud the exercise

--- src/components/child/Submission.tsx
+++ src/components/child/Submission.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { IconWritingSign, IconCloudUpload } from "@tabler/icons-react";
+import { IconWritingSign, IconCloudUpload, IconVolume } from "@tabler/icons-react";
 import classes from "./Submission.module.css";
 import Label from "../ui/label/Label";
 import Button from "../ui/button/Button";
@@ -9,12 +9,28 @@ import useExerciseStore from "../../store/useExerciseStore";
 
 const buttonsStyle = { width: "9rem", height: "3rem", fontSize: "1rem" };
 
+
 const Submission = () => {
   const [selectedImage, setSelectedImage] = useState("");
-  const {requested_text, setExercise} = useExerciseStore()
+  
+  const {requested_text, level, category, setExercise} = useExerciseStore()
   const cameraInputRef = useRef<HTMLInputElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
-
+  // if speechSynthesis is not supported, the volume button will not be rendered
+  // TODO there are two options to enum for the level(start with capital letter or not) - discuss
+  const readExercise = () => {
+    if (!("speechSynthesis" in window)) return;
+    let guidingText = "";
+    if(level == "letters")
+      // TODO maybe state if the letter is uppercase or lowercase
+      guidingText = "Please write the letter " + requested_text[0] + requested_text.length + " times.";
+    else if(level == "words")
+      guidingText = requested_text
+    else if(level == "category")
+      guidingText = "Please write a word from the category: " + category;
+    const utterance = new SpeechSynthesisUtterance(guidingText);
+    speechSynthesis.speak(utterance);
+  };
   // will be used again after the exercise is submitted
   // TODO add some loading state here - if not - will need to clear exercise store after logout
   const fetchExercise = async () => {
@@ -50,12 +66,18 @@ const Submission = () => {
       fileInputRef.current.click();
     }
   };
-
   return (
     <section className={classes.submission}>
       <div>
         <IconWritingSign stroke={2} size={40} />
         <Label>{requested_text}</Label>
+        {('speechSynthesis' in window) && (
+        <IconVolume
+            className={classes.volumeIcon}
+            size={35}
+            onClick={() => readExercise()}
+          />
+        )}
       </div>
       <div>
         <IconCloudUpload stroke={2} size={40} />
