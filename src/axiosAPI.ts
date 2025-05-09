512890020d12c495bb3f3aed3ffe085c08a458d2
Ofir Beck <ofirbeck987@gmail.com>
2025-05-09 17:12:17 +0300

    build: updated axios request canceltion time

--- src/axiosAPI.ts
+++ src/axiosAPI.ts
@@ -6,7 +6,7 @@ const baseURL = import.meta.env.VITE_APP_API_URL || 'http://127.0.0.1:8000/'
 
 const axiosAPI = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
