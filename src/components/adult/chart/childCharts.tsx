e70fab9717b76f6c3eead21597738c38ab46a4a2
Ofir Beck <ofirbeck987@gmail.com>
2025-06-11 15:47:05 +0300

    feat: added daily progression and often confused letters charts

--- src/components/adult/chart/childCharts.tsx
+++ src/components/adult/chart/childCharts.tsx
@@ -1,10 +1,11 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LineChart, Line } from 'recharts';
 import Label from "../../ui/label/Label";
 import classes from "./childCharts.module.css";
 import axiosAPI from "../../../axiosAPI";
 import useChildStore from "../../../store/useChildStore";
 import { useEffect, useState } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
+import { C } from 'vitest/dist/chunks/reporters.d.79o4mouw.js';
 
 interface Letter {
     letter: string;
@@ -16,11 +17,26 @@ interface Level {
     avg_score: number;
 }
 
+interface DailyStats {
+    day: string;
+    avg_score: number;
+    exercise_count: number;
+}
+
+interface ConfusedLetter {
+    letter: string,
+    confused_with: string,
+    times: number,
+    confusion_percentage: number
+}
+
+
 const ChildCharts = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [letterStats, setLetterStats] = useState<Letter[]>([]);
     const [levelStats, setLevelStats] = useState<Level[]>([]);
-
+    const [dailyStats, setDailyStats] = useState<DailyStats[]>([]);
+    const [confusedLetters, setConfusedLetters] = useState<ConfusedLetter[]>([]);
     useEffect(() => {
         const fetchLetters = async () => {
             try {
@@ -32,6 +48,8 @@ const ChildCharts = () => {
                 console.log(response.data);
                 setLetterStats(response.data.letter_scores);
                 setLevelStats(response.data.level_scores);
+                setDailyStats(response.data.daily_scores);
+                setConfusedLetters(response.data.often_confused_letters);
             } catch (error: any) {
                 console.error(
                     "Failed to fetch letter stats",
@@ -64,6 +82,53 @@ const ChildCharts = () => {
                             />
                         </BarChart>
                     </ResponsiveContainer>}
+            <Label>Daily Progression</Label>
+            {isLoading ? <ClipLoader className={classes.clipLoader} /> :
+                dailyStats.length === 0 ? <Label>No Daily Progress...</Label> :
+                    <ResponsiveContainer className={classes.chartContainer}>
+                        <LineChart data={dailyStats}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="day" />
+                        <YAxis />
+                        <Tooltip
+                        labelFormatter={(label, payload) => {
+                            const exerciseCount = payload[0]?.payload?.exercise_count;
+                            return `Date: ${label} (Exercised ${exerciseCount} Exercises)`;
+                        }}
+                        />
+                        <Line
+                            type="monotone"
+                            dataKey="avg_score"
+                            stroke="#2A004E"
+                        />
+                        </LineChart>
+                    </ResponsiveContainer>}
+            <Label>Often Confused Letters</Label>
+            {isLoading ? <ClipLoader className={classes.clipLoader} /> :
+                confusedLetters.length === 0 ? (
+                    <Label>No Confused Letters...</Label>
+                ) : (
+                    <ResponsiveContainer className={classes.chartContainer}>
+                        <BarChart data={confusedLetters}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis type="category"
+                            dataKey={(entry) => `${entry.letter} was confused with ${entry.confused_with}`}/>
+                            <YAxis />
+                            <Tooltip
+                               formatter={(value, name, entry) => {
+                                    const times = entry?.payload?.times;
+                                    return [
+                                        `Confusion Percentage: ${value} (Confused ${times} times)`,
+                                    ];
+                                }}/>
+                            <Bar
+                                fill="#2A004E"
+                                dataKey="confusion_percentage"
+                                radius={[4, 4, 0, 0]}
+                            />
+                        </BarChart>
+                    </ResponsiveContainer>
+                )}            
             <Label>Progress By Level</Label>
             {isLoading ? <ClipLoader className={classes.clipLoader} /> :
                 levelStats.length === 0 ? (
