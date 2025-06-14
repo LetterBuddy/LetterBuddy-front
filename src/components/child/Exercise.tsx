8658fc535bdd72a1b24607bdb13ada833cb71a9b
Ofir Beck <ofirbeck987@gmail.com>
2025-06-14 10:30:31 +0300

    fix: returned missing letter excercises showing in excercise

--- src/components/child/Exercise.tsx
+++ src/components/child/Exercise.tsx
@@ -160,7 +160,7 @@ const Submission = () => {
       <section className={classes.submission}>
         <div>
           <IconWritingSign stroke={2} size={40} className={classes.writingIcon} />
-          {level == "words" &&
+          {(level == "words" || level == "letters") &&
             <Label style={{ fontSize: "2.2rem", marginTop: "1.5rem" }}>{requested_text}</Label>}
           {level == "category" &&
           <section className={classes.categorySection}>
